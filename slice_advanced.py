7f5ac6790104c9b058f0f91f8e23f92f084d8965
Andy <andy@lancito01.dev>
2025-08-29 12:44:07 -0300

    fix slice advanced

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,7 +1,9 @@
 def slice_advanced():
     # Código a implementar utilizando input.
     text = input("Ingresar un texto:\n> ")
-    print(text[5::2])
+    print(text[4::2])
+
+slice_advanced()
 
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
