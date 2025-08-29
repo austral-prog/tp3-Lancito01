8fbb6a999aadbd7435d3f2958ba2119c4598b856
Andy <andy@lancito01.dev>
2025-08-29 12:52:17 -0300

    fix in_string.py
    el print multilínea no le gusta al test 😐

--- in_string.py
+++ in_string.py
@@ -1,17 +1,12 @@
 def check_vowels():
     # Código a implementar utilizando input.
-    name = input("Ingrese un nombre:\n> ")
+    name = input("Ingrese un nombre:\n> ").lower()
 
-    print(
-        f"""
-Contiene a: {"a" in name.lower()}
-Contiene e: {"e" in name.lower()}
-Contiene i: {"i" in name.lower()}
-Contiene o: {"o" in name.lower()}
-Contiene u: {"u" in name.lower()}
-"""
-    )
-
+    print(f"Contiene a: {"a" in name}")
+    print(f"Contiene e: {"e" in name}")
+    print(f"Contiene i: {"i" in name}")
+    print(f"Contiene o: {"o" in name}")
+    print(f"Contiene u: {"u" in name}")
 
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_in_string_test.py` o `python tp3_in_string_test.py`
--- slice_advanced.py
+++ slice_advanced.py
@@ -3,7 +3,5 @@ def slice_advanced():
     text = input("Ingresar un texto:\n> ")
     print(text[4::2])
 
-slice_advanced()
-
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
