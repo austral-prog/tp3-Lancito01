c057302fa79a135e82f7814086ecfcc9d67533a4
Andy <andy@lancito01.dev>
2025-09-02 10:48:01 -0300

    Fix quotation marks in vowel check prints

--- in_string.py
+++ in_string.py
@@ -1,11 +1,11 @@
 def check_vowels():
     name = input("Ingrese un nombre:\n> ").lower()
 
-    print(f"Contiene a: {"a" in name}")
-    print(f"Contiene e: {"e" in name}")
-    print(f"Contiene i: {"i" in name}")
-    print(f"Contiene o: {"o" in name}")
-    print(f"Contiene u: {"u" in name}")
+    print(f"Contiene a: {'a' in name}")
+    print(f"Contiene e: {'e' in name}")
+    print(f"Contiene i: {'i' in name}")
+    print(f"Contiene o: {'o' in name}")
+    print(f"Contiene u: {'u' in name}")
 
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_in_string_test.py` o `python tp3_in_string_test.py`
